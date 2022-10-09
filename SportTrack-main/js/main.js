f69e178b11c61a321c8dc959c06fde9a1e7573fd
yamakajump <114985610+yamakajump@users.noreply.github.com>
2022-10-09 14:16:23 +0200

    Add files via upload

--- SportTrack-main/js/main.js
+++ SportTrack-main/js/main.js
@@ -10,8 +10,8 @@
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -21,7 +21,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -31,14 +31,45 @@
         $('.sidebar, .content').toggleClass("open");
         return false;
     });
-
-
-    // Progress Bar
-    $('.pg-bar').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
-    
 })(jQuery);
 
+//form signUp
+function valideSingUp() {
+    // Sélectionner l'élément input et récupérer sa valeur
+    var ret = false;
+
+    var singUp_Nom = document.getElementById("singUp_Nom").value;
+    var singUp_Prenom = document.getElementById("singUp_Prenom").value;
+    var singUp_Date = document.getElementById("singUp_Date").value;
+    var singUp_Sexe = document.getElementById("singUp_Sexe").value;
+    var singUp_Taille = document.getElementById("singUp_Taille").value;
+    var singUp_Poids = document.getElementById("singUp_Poids").value;
+    var singUp_Email = document.getElementById("singUp_Email").value;
+    var singUp_Password = document.getElementById("singUp_Password").value
+
+    var text = "Bonjour " + singUp_Nom + " " + singUp_Prenom + "\rVoici les informations sur vous :\rDate de naissance : " + singUp_Date + "\rSexe : " + singUp_Sexe + "\rTaille : " + singUp_Taille + "\rPoids : " + singUp_Poids + "\r\rIdentifiant :\rMail : " + singUp_Email + "\rMot de passe : " + singUp_Password + "\r\rPoru vous inscrire appuyez sur \"Ok\""
+    var conf = confirm(text);
+    if (conf) {
+        alert("Votre inscription a bien été pris en compte, vous pouvez maintenant vous connecté");
+        document.location.href="signin.html";
+        ret = true;
+    } else {
+        alert("Votre inscription n'a pas été faite comme à votre demande");
+    }
+    return ret;
+}
+
+//vérifiction mot de passe identique
+var singUp_Password = document.getElementById("singUp_Password");
+var singUp_confirmPassword = document.getElementById("singUp_confirmPassword");
+
+function validatePassword() {
+    if (singUp_Password.value != singUp_confirmPassword.value) {
+        singUp_confirmPassword.setCustomValidity("Passwords Don't Match");
+    } else {
+        singUp_confirmPassword.setCustomValidity('');
+    }
+}
+
+singUp_Password.onkeyup = validatePassword;
+singUp_confirmPassword.onkeyup = validatePassword;
